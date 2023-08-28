223b30764db476332a4d014d9515456bf27d8401
amac81 <arnaldocanelas@gmail.com>
2023-08-28 16:43:50 +0100

    project update

---
+++ src/main/java/pt/bitclinic/javasbmvc01/Javasbmvc01Application.java
@@ -0,0 +1,13 @@
+package pt.bitclinic.javasbmvc01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javasbmvc01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Javasbmvc01Application.class, args);
+	}
+
+}
