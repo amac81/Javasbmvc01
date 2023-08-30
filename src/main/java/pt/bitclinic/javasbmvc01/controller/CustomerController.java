a34b72200708023643b0059f2971587786a03154
amac81 <arnaldocanelas@gmail.com>
2023-08-30 16:01:27 +0100

    customer form update

--- src/main/java/pt/bitclinic/javasbmvc01/controller/CustomerController.java
+++ src/main/java/pt/bitclinic/javasbmvc01/controller/CustomerController.java
@@ -1,9 +1,12 @@
 package pt.bitclinic.javasbmvc01.controller;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -12,27 +15,33 @@ import pt.bitclinic.javasbmvc01.entities.Customer;
 
 @Controller
 public class CustomerController {
-	
+
+	// Pre-process all web requests coming into our Controller
+	// Pre-process every String form data; remove leading and trailing white space
+	// if String only has white space... "trim" it to null
+	@InitBinder
+	public void initBinder(WebDataBinder webDataBinder) {
+		// removes whitespaces - leading and trailing
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+		webDataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
+
 	@GetMapping("/")
 	public String showForm(Model theModel) {
 		theModel.addAttribute("customer", new Customer());
 
 		return "customer-form";
 	}
-	
+
 	@PostMapping("/processForm")
-	public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer, BindingResult theBindingResult) {		
-		
-		if(theBindingResult.hasErrors()) {
+	public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer, BindingResult theBindingResult) {
+
+		if (theBindingResult.hasErrors()) {
 			return "customer-form";
+		} else {
+			return "customer-confirmation";
 		}
-		else {
-			
-			return "customer-confirmation";			
-		}
-			
-			
-		
 	}
-		
+
 }
--- src/main/java/pt/bitclinic/javasbmvc01/entities/Customer.java
+++ src/main/java/pt/bitclinic/javasbmvc01/entities/Customer.java
@@ -3,6 +3,8 @@ package pt.bitclinic.javasbmvc01.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -11,11 +13,18 @@ public class Customer implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
+	
+	@NotNull(message = "is required")
+	@Size(min = 1, message = "is required")
 	private String firstName;
 
 	@NotNull(message = "is required")
 	@Size(min = 1, message = "is required")
 	private String lastName;
+	
+	@Min(value = 0, message = "must be greater than or equal to zero")
+	@Max(value = 10, message = "must be less than or equal to ten")
+	private int freePasses;	
 
 	public Customer() {
 	}
@@ -34,6 +43,14 @@ public class Customer implements Serializable {
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+
+	public int getFreePasses() {
+		return freePasses;
+	}
+
+	public void setFreePasses(int freePasses) {
+		this.freePasses = freePasses;
 	}
 
 	@Override
@@ -55,7 +72,8 @@ public class Customer implements Serializable {
 
 	@Override
 	public String toString() {
-		return "Customer [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+		return "Customer [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", freePasses="
+				+ freePasses + "]";
 	}
 
 }
